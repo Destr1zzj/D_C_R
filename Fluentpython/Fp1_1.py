f307969b7e002f2c5a82220783bc2e9638e7e703
Destr1zzj <deszzj@gmail.com>
2022-01-03 20:50:13 +0800

    1.爬取微信文章工具 2.FP学习文件

--- Fluentpython/Fp1_1.py
+++ Fluentpython/Fp1_1.py
@@ -6,6 +6,7 @@ class FrenchDeck():
     ranks = [str(n) for n in range(2,11)]+ list('JQKA')
     suits = 'spades diamonds clubs hearts'.split()
 
+
     def __init__(self):
         self._cards = [Card(rank,suit) for suit in self.suits
                                         for rank in self.ranks]
@@ -14,6 +15,12 @@ class FrenchDeck():
 
     def __getitem__(self, position):
         return self._cards[position]
+
+
+suit_values = dict(spades = 3,hearts = 2,diamonds = 1,clubs = 0)
+def spades_high(card):
+    rank_value =FrenchDeck.ranks.index(card.rank)
+    return rank_value*len(suit_values) + suit_values[card.suit]
 
 if __name__ == '__main__':
     pass
---
+++ Fluentpython/Fp1_2.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# python:3
+from math import hypot
+
+
+class Vector:
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+
+    def __repr__(self):
+        return 'Vector({0},{1})'.format(self.x, self.y)
+
+    def __abs__(self):
+        return hypot(self.x, self.y)
+
+    def __bool__(self):
+        return bool(abs(self))
+
+    def __add__(self, other):
+        x = self.x + other.x
+        y = self.y + other.y
+        return Vector(x, y)
+
+    def __mul__(self, other):
+        return Vector(self.x*other, self.y*other)
